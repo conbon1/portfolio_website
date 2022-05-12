71ef0e63f26f593237c44c3494094b6a3cc9c4f6
conbon1 <connor.leiting@gmail.com>
2022-05-11 23:09:33 -0500

    commit512

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -12,8 +12,8 @@ const Hero = (props) => (
           The Wizard Worlds Project
         </SectionTitle></center>
         <center><SectionText>
-          Genesis Wizard mint is coming soon<br />
-          follow our twitter to stay up to date!
+          Genesis Wizard mint is coming soon...<br />
+          Follow our twitter to stay up to date!
         </SectionText></center>
         <center><Button onClick={props.handleClick}>
           <a href='https://www.launchmynft.io/collections/AETMcjSATqwGHFJvPdZFnBdJzKfbhHW4H3RAcLsan8ZH/1XewNW3yLtie2EXkr1Kj'>Mint</a>
